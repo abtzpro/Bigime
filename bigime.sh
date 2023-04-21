9569096434f81f04a828abb06dc844282d3dbc00
Adam R <121298839+abtzpro@users.noreply.github.com>
2023-04-21 17:34:31 -0500

    Create bigime.sh

---
+++ bigime.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Banner
+echo "██████╗ ██╗ ██████╗ ██╗███╗   ███╗███████╗"
+echo "██╔══██╗██║██╔════╝ ██║████╗ ████║██╔════╝"
+echo "██████╔╝██║██║  ███╗██║██╔████╔██║█████╗  "
+echo "██╔══██╗██║██║   ██║██║██║╚██╔╝██║██╔══╝  "
+echo "██████╔╝██║╚██████╔╝██║██║ ╚═╝ ██║███████╗"
+echo "╚═════╝ ╚═╝ ╚═════╝ ╚═╝╚═╝     ╚═╝╚══════╝"
+echo "          Developed by abtzpro and Hello Security"
+echo ""
+
+# Install packages
+echo "Installing packages..."
+apt update
+apt install -y nmap hydra john wireshark aircrack-ng sqlmap
+echo "Packages installed successfully!"
+echo ""
+
+# Finish
+echo "PocketToolz installation complete!"
